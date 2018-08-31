850e7b4d3e6d67bae62571a211c726e9f14362a9
Arnab Das <arnab@coderuse.com>
2018-08-31 08:21:16 +0000

    Using ready event to push messages

--- app.js
+++ app.js
@@ -6,19 +6,30 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        
         navigator.serviceWorker.register('./sw.js').then(function (registration) {
             // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
-
-            setTimeout(function (){
-                navigator.serviceWorker.controller.postMessage({
-                    message: 'Hello, World!!!'
-                });
-            }, 3000);
         }, function (err) {
             // registration failed :(
             console.log('ServiceWorker registration failed: ', err);
         });
     });
+
+    navigator.serviceWorker.ready.then(function (serviceWorkerRegistration) {
+        // Let's see if you have a subscription already
+        return serviceWorkerRegistration.pushManager.getSubscription();
+    }).then(function (subscription) {
+        if (!subscription) {
+            console.log('You do not have subscription');
+        }
+        // You have subscription.
+        // Send data to service worker
+        setTimeout(function () {
+            navigator.serviceWorker.controller.postMessage({
+                message: 'Hello, World!!!'
+            });
+        }, 3000);
+    })
 }
+else
+    console.log('Service Worker is not supported by your browser!!!');
